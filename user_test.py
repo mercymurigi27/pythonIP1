5926163226b92f414185df39830577a6a13cbc01
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-04-26 07:32:11 +0300

    USER CACHE

--- user_test.py
+++ user_test.py
@@ -1,6 +1,7 @@
 import unittest
 from User import user_acc
 
+
 class Testuser_acc(unittest.TestCase):
 
     """
@@ -10,11 +11,57 @@ def setUp(self):
 
     self.new_User = user_acc("mercymurigi","mercy1234","murigi@yahoo.com")    
 
+
+def tearDown(self):
+        '''
+        tearDown method that cleans up after every test
+        '''
+
+        user_acc.User_accounts_list = []    
+
 def test_init(self):
 
     self.assertEqual(self.new_User.user_name,"mercymurigi")
     self.assertEqual(self.new_User.passward,"mercy123")
     self.assertEqual(self.new_User.email,"murigi@yahoo.com")
+
+
+def test_save_User(self):
+        '''
+        test_save_user tests case to find out if the user object is being saved
+        '''
+
+        self.new_User.save_user_acc()
+        self.assertEqual(len(user_acc.User_accounts_list),1)
+
+
+def test_delete_User(self):
+        '''
+         test if we can remove a user from out user accounts list
+        '''
+
+
+        self.new_User.save_User()
+        test_user = user_acc('Test', 'user_name', 'mercy1234', 'test@user.com')
+        test_user.save_user_acc()
+
+        self.new_User.delete_User()
+
+        self.assertEqual(len(user_acc.User_accounts_list),1)
+
+
+
+def test_find_user_by_username(self):
+        '''
+        test to check if we can find a contact by username and display information
+        '''
+
+        self.new_User.save_user_acc()
+        test_user = user_acc('Test', 'user_name', 'mercy1234', 'test@user.com')
+        test_user.save_user_acc()
+
+        found_user = user_acc.find_by_username('user_name')
+        self.assertEqual(found_user.email, test_user.email)
 
 
 
